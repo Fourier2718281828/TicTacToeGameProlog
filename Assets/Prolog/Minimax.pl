a445d4e38b46519ba819cf01be5efffdd3987ac6
Fourier2718281828 <non-linear@ukr.net>
2023-04-09 06:12:18 +0300

    +almost win check pl

--- Assets/Prolog/Minimax.pl
+++ Assets/Prolog/Minimax.pl
@@ -1,50 +1,3 @@
-% % Define the dynamic predicate person/1
-% :- dynamic person/1.
-
-% % Define the existing database of people
-% person(dave).
-% person(mark).
-% person(holly).
-
-% % Define the add_person predicate to add a new person
-% add_person(Name) :-
-%     % Check if the person already exists
-%     (person(Name) ->
-%         write(Name), write(' is already in the database');
-%     % If not, add the person to the database
-%         assertz(person(Name)),
-%         write(Name), write(' has been added to the database')),
-%     nl. % Print a newline for formatting purposes
-
-
-%next_board(X, 123413).
-
-
-%%%%%%%%%%%%%%%%%
-
-% % Define a predicate `victory_row(Board, M, Player)` that checks if there are three cells in a row on the Board with the same value Player (1 or -1).
-% victory_row1(Board, M, N, Row) :-
-%     Start is 0,
-%     End is M * N,
-%     findall(Cell, (nth0(Index, Board, Cell), between(Start, End, Index)), Row).
-%     %length(Row, N),
-%     %maplist(=(Player), Row).
-
-% % Define a predicate `victory_col(Board, N, Player)` that checks if there are three cells in a column on the Board with the same value Player (1 or -1).
-% victory_col1(Board, N, Player) :-
-%     findall(Cell, (nth0(Index, Board, Cell), Index mod N =:= N - 1), Col),
-%     length(Col, N),
-%     maplist(=(Player), Col).
-
-% % Define a predicate `victory_diag(Board, Player)` that checks if there are three cells in a diagonal on the Board with the same value Player (1 or -1).
-% victory_diag1(Board, N, Player) :-
-%     findall(Cell, (nth0(Index, Board, Cell), Index mod (N+1) =:= 0), Diag1),
-%     length(Diag1, L),
-%     findall(Cell, (nth0(Index, Board, Cell), Index mod (N-1) =:= N-1), Diag2),
-%     length(Diag2, L),
-%     maplist(=(Player), Diag1),
-%     maplist(=(Player), Diag2).
-
 :- dynamic cross/1.
 :- dynamic naught/1.
 :- dynamic empty/1.
@@ -72,50 +25,74 @@ grid_index(Index, I, J) :-
         J is Index mod N
     ).
 
-find_win_sequence(_, [], _, _, _, _).
-find_win_sequence(Board, [Empty | Cells], Index, Nxt, K, Seq) :- 
-    empty(Empty), 
-    NxtIndex is Index + 1,
-    find_win_sequence(Board, Cells, NxtIndex, Nxt, K, Seq).
-find_win_sequence(Board, [Cell | _], Index, Nxt, K, Seq) :-
-    find_win_seq_util(Board, Index, Nxt, K, Cell, [Index], [], Seq).
+apply(PArgs, AdditionalArgs) :-
+    append(PArgs, AdditionalArgs, FullPArgs),
+    Call =.. FullPArgs,
+    call(Call).
 
-find_win_seq_util(_, _, _, 0, _, CurrSeq, SeqAcum, Seq) :- append(SeqAcum, [CurrSeq], Seq).
-find_win_seq_util(Board, StartIndex, Nxt, K, Player, CurrSeq, SeqAcum, Seq) :-
-    call(Nxt, StartIndex, NxtIndex),
-    nth0(NxtIndex, Board, Player),
-    NxtK is K - 1,
-    append(CurrSeq, [NxtIndex], NewCurrSeq),
-    find_win_seq_util(Board, NxtIndex, Nxt, NxtK, Player, NewCurrSeq, SeqAcum, Seq).
+for_each(PArgs, List, AccIn, AccOut) :- 
+    for_each_util(PArgs, List, 0, AccIn, AccOut).
+
+for_each_util(_, [], _,  Acc, Acc).
+for_each_util(PArgs, [Elem|Elems], Index, AccIn, AccOut) :-
+    apply(PArgs, [Elem, Index, AccIn, AccNext]),
+    NextIndex is Index + 1,
+    for_each_util(PArgs, Elems, NextIndex, AccNext, AccOut),!.
+
+find_win_sequence(OrigBoard, NextIndexPArgs, IndTripletCheckPArgs, Elem, Index0, AccIn, AccNext) :- 
+    (
+        empty(Empty),
+        Elem \= Empty,
+        apply(NextIndexPArgs, [Index0, Index1]),
+        apply(NextIndexPArgs, [Index1, Index2]),
+        apply(IndTripletCheckPArgs, [Index0, Index1, Index2]),
+        nth0(Index0, OrigBoard, Elem),
+        nth0(Index1, OrigBoard, Elem),
+        nth0(Index2, OrigBoard, Elem),
+        AccNext = [[Index0, Index1, Index2] | AccIn]
+    );
+    AccNext = AccIn.
 
 shift_rght(Index, NxtIndex) :- NxtIndex is 1 + Index.
 shift_down(Index, NxtIndex) :- n(N), NxtIndex is N + Index.
 shift_diag(Index, NxtIndex) :- n(N), NxtIndex is 1 + N + Index.
 
-all_sequences(Board, Nxt, K, Seq) :-
-    ReducedK is K - 1,
-    find_win_sequence(Board, Board, 0, Nxt, ReducedK, Seq).
+within_one_row(Index0, Index1, Index2) :-
+    grid_index(Index0, I, _),
+    grid_index(Index1, I, _),
+    grid_index(Index2, I, _).
+
+within_one_col(Index0, Index1, Index2) :-
+    grid_index(Index0, _, J),
+    grid_index(Index1, _, J),
+    grid_index(Index2, _, J).
+
+within_one_dgn(_, _, _) :- true.
+
+all_victory_sequences(Board, ShiftPArgs, CheckIndexTripletPArgs, Sequences) :-
+    PArgs = [find_win_sequence, Board, ShiftPArgs, CheckIndexTripletPArgs],
+    for_each(PArgs, Board, [], Sequences).
 
 all_victory_rows(Board, Rows) :-
-    all_sequences(Board, shift_rght, 3, Rows).
+    all_victory_sequences(Board, [shift_rght], [within_one_row], Rows).
 
 all_victory_cols(Board, Cols) :-
-    all_sequences(Board, shift_down, 3, Cols).
+    all_victory_sequences(Board, [shift_down], [within_one_col], Cols).
 
 all_victory_dgns(Board, Dgns) :-
-    all_sequences(Board, shift_diag, 3, Dgns).
+    all_victory_sequences(Board, [shift_diag], [within_one_dgn], Dgns).
 
 % victory_row (Board, M, N, Winner).
 % victory_col (Board, M, N, Winner).
 % victory_diag(Board, M, N, Winner).
 victory_row(_, 2) :- false.
-victory_col(_, 2) :- false.
-victory_dgn(_, 2) :- false.
+victory_col(_, 1) :- true.
+victory_dgn(_, 3) :- true.
 
 winner(Board, Winner) :- 
-    victory_row(Board, Winner); 
-    victory_col(Board, Winner); 
-    victory_dgn(Board, Winner).
+    victory_row(Board, Winner),!; 
+    victory_col(Board, Winner),!; 
+    victory_dgn(Board, Winner),!.
 
 victory(Board, Winner) :-
     cross(Cross),
